c89de65eb681ae35ba4277d181d0a82d06aac891
Kunwar Vijay Saw <kunwarvijay54@gmail.com>
2026-04-23 21:51:19 +0530

    updated

--- JAVA/src/MockJava_Array/Duplicate.java
+++ JAVA/src/MockJava_Array/Duplicate.java
@@ -3,7 +3,7 @@ package MockJava_Array;
 public class Duplicate {
 
 	public static void main(String[] args) {
-		int arr[]= {1,4,3,4,2,1,3};
+		int arr[]= {1,4,3,1,4,2,1,3};
 		
 		for(int i=0;i<arr.length;i++) {
 			int count=1;
@@ -19,9 +19,11 @@ public class Duplicate {
 				}
 			
 			}
-			if(count>=1 && found==0) {
+			//Duplicate elements
+			if(count>1 && found==0) {
 				System.out.println(arr[i]+"->"+count);
 			}
+			s
 		}
 	}
 
