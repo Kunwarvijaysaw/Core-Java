2f30bc21820a93f1f431e8ca79db42efee5d5ec0
Kunwar Vijay Saw <kunwarvijay54@gmail.com>
2026-06-21 23:08:38 +0530

    updated

--- JAVA/src/SiftAllZeroInLast.java
+++ JAVA/src/SiftAllZeroInLast.java
@@ -4,13 +4,25 @@ public class SiftAllZeroInLast {
 	public static void main(String[] args) {
 		int arr[] = { 1, 0, 5, 3, 2, 0, 9 };
 		int j = 0;
+//		for (int i = 0; i < arr.length; i++) {
+//			if (arr[i] != 0) {
+//				int temp = arr[i];
+//				arr[i] = arr[j];
+//				arr[j] = temp;
+//				j++;
+//			}
+//		}
+		
 		for (int i = 0; i < arr.length; i++) {
 			if (arr[i] != 0) {
-				int temp = arr[i];
-				arr[i] = arr[j];
-				arr[j] = temp;
+				arr[j] = arr[i];
 				j++;
 			}
+		}
+		
+		while(j<arr.length) {
+			arr[j]=0;
+			j++;
 		}
 
 		for (int num : arr) {
